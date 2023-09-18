6a4663cb527a45c82cf02e637fd888829fc9abc4
MrArcano <salvatore-dario@hotmail.it>
2023-09-18 16:02:55 +0200

    [edit]: style

--- js/script.js
+++ js/script.js
@@ -2,37 +2,44 @@ const btnGen = document.getElementById("btn-gen");
 const containerBox = document.getElementById("container-box");
 const dimBox = document.getElementById("input-dim");
 
+init();
+
 btnGen.addEventListener("click",function(){
     // 1. resetContainer
   containerBox.innerHTML="";
 
-  // seleziono dimBox
-  const countBox = dimBox.value ** 2;
+  init();
 
-  // 2. Add box
-  for(let i=1; i<=countBox; i++){
-    // richiamo la creazione di un box
-    const box = createBox(i,dimBox.value);
-
-    // per ogni box aggiungo un eventListner CLICK
-    box.addEventListener("click", function(){
-      console.dir(this);
-      console.log(this._boxID);
-      // aggiungo o rimuovo la classe clicked per il cambio del bg
-      this.classList.toggle("clicked");
-    })
-    
-    // append del box, al containerBox
-    containerBox.append(box);
-  };
 });
 
 // ************** FUNCTION **************
 function createBox(index,dimBox){
   const newBox = document.createElement("div");
   newBox.className = "box";
-  newBox.style.width = `calc(100% / ${dimBox})`;
+  // newBox.style.width = `calc(100% / ${dimBox})`;
+  newBox.style.width = 100 / dimBox + "%";
   // newBox.innerHTML = index;
   newBox._boxID = index;
   return newBox;
+}
+
+function init(){
+    // seleziono dimBox
+    const countBox = dimBox.value ** 2;
+    // 2. Add box
+    for(let i=1; i<=countBox; i++){
+      // richiamo la creazione di un box
+      const box = createBox(i,dimBox.value);
+  
+      // per ogni box aggiungo un eventListner CLICK
+      box.addEventListener("click", function(){
+        console.dir(this);
+        console.log(this._boxID);
+        // aggiungo o rimuovo la classe clicked per il cambio del bg
+        this.classList.toggle("clicked");
+      })
+      
+      // append del box, al containerBox
+      containerBox.append(box);
+    };
 }
